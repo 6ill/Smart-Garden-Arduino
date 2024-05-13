8b45e14d44e014413b37e4c6fa964ec810cd7662
6ill <kyrbill123@gmail.com>
2024-05-13 19:05:36 +0700

    minor changes

--- python/yolo.py
+++ python/yolo.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import cv2
 import numpy as np
+import time
 from database import connect_database, insert_mouse_check_data
 
 conn = connect_database()
@@ -15,3 +16,4 @@ for r in results:
         print("MOUSE DETECTED!!")
     else:
         insert_mouse_check_data(conn, '0')
+    time.sleep(0.1)
